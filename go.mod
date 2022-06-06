3d613cc5ae5ba1baac49cfced302ec255c6c4691
dancohen2022 <102294228+dancohen2022@users.noreply.github.com>
2022-06-06 16:54:09 +0300

    Update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dancohen2022/Mongo-Db-Go
 
 go 1.18
+
+require go.mongodb.org/mongo-driver v1.9.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
+	"github.com/mongodb/mongo-go-driver"
+	"github.com/mongodb/mongo-go-driver/mongo/primitive"
+)
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id, omitempty" bson:"_id, omitempty"`
+	FirstName string             `json:"firstname, omitempty" bson:"firstname, omitempty"`
+	LastName  string             `json:"lasttname, omitempty" bson:"lasttname, omitempty"`
+}
+
+var client *mongo.client
+
+func main() {
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, "mongodb://localhost:27017")
+	router := mux.NewRouter()
+	http.ListenAndServe(":12345", router)
 }
